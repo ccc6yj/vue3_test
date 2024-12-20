f91555b17b8716be5c428ec6e818bb7744992c4d
yjliuds <yjliuds@isoftstone.com>
2024-12-20 20:42:31 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app">
+      <h1>你好啊！</h1>
+      <Person/>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import Person from './componens/Person.vue';
+    export default {
+      name:'App', //组件名
+      components:{Person}
+    }
+  </script>
+  
+  <style>
+    .app {
+      background-color: #ddd;
+      box-shadow: 0 0 10px;
+      border-radius: 10px;
+      padding: 20px;
+    }
+  </style>
